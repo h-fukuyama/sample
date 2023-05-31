45eaf789be8a36adc77eb32e58f4d35ee0b00cc3
h-fukuyama <h-fukuyama@usen-next.jp>
2023-05-31 15:28:05 +0900

    third

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,5 +1,6 @@
 //functionの設定
 function calcBmi() {
+    //変数の設定
     var height = document.getElementById("height").value
     var weight = document.getElementById("weight").value
     var bmi = weight / Math.pow(height / 100, 2)
